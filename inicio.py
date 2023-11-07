7adc221d951f5e4df58b92d276a882c3f346b780
Ana <acgs@cesar.school>
2023-11-07 20:57:09 -0300

    sucumba

--- inicio.py
+++ inicio.py
@@ -3,3 +3,7 @@ os.system ("cls")
 
 # Ideia inicial do dicionario:
 # livros = { 'Nome_do_Livro': ['Categoria', 'Autor', Preço]}
+
+biblioteca = {'Ordem Paranormal Iniciação': ['Graphic Novel', 'Rafael Lange', 129.00]}
+
+print(biblioteca)
