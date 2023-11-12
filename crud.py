82340b00485a5c63d089ef92b70bca18b5320705
Ana <acgs@cesar.school>
2023-11-12 15:14:52 -0300

    Função atualizar criada e funcionando

--- crud.py
+++ crud.py
@@ -2,9 +2,12 @@ import os
 os.system("cls")
 
 # livros = { 'Nome_do_Livro': ['Categoria', 'Autor', Preço]}
-biblioteca = {}
-file = open("teste.txt", "a")
 
+#Dicionatio biblioteca - Atualmente vazio - Usuario insere as informações
+biblioteca = {'Herding Cats':['Quadrinhos', 'Sarah Andersen', 30.00]}
+
+
+#Função menu ainda não funciona corretamente (while true infinito)
 def menu():
     
     while True:
@@ -14,18 +17,44 @@ def menu():
 
         if i == 0:
             break
+        elif i == 1:
+            return cadastro()
 
 
+#Função de Create/Cadastro de Livros
 def cadastro():
 
     nome = input("Nome do livro: ")
-    categoria = input("Categoria do livro: ")
+    categoria = input("Categoria do livro: ") #Atualmente só suportamos 1 categoria
     autor = input("Autor do livro: ")
-    valor = float(input("Nome do livro: "))
-
+    valor = float(input("Valor gasto no livro: "))
     biblioteca[nome] = [categoria, autor, valor]
 
+# def read(): #Atualmente sem ideia de como executar
+
+
+#Função Update/Atualizar informações de livros
+def update():
+
+    print("Qual livro deseja alterar as informações: ")
+    key = input("livro: ")
+    print("Secção de alterar informações, qual informação deseja alterar? \n1- Alterar Categoria \n2- Alterar Autor \n3- Alterar valor")
+    seccao = int(input())
+    print("escreva a informação nova: ")
+    value = input()
+
+    if seccao == 3:
+        value = float(value)
+
+    for i in range(len(biblioteca[key])):
+        
+        if i+1 == seccao:
+            biblioteca[key][i] = value
+
+
+#--------------------------------------------------------
 cadastro()
-file.write(biblioteca.keys())
-file.close()
+print(biblioteca)
+
+update()
 print(biblioteca)
