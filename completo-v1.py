b8b56a0fde09ef1052b103a222ae567176a9d166
Ana <acgs@cesar.school>
2023-11-22 15:51:40 -0300

    prints bonitos

--- completo-v1.py
+++ completo-v1.py
@@ -109,7 +109,7 @@ def cadastro_livros():
 
         elif nome not in biblioteca:
             
-            categoria = input("Categoria do livro: ").title() #Atualmente só suportamos 1 categoria
+            categoria = input("Gênero do livro: ").title() #Atualmente só suportamos 1 genero
             autor = input("Autor do livro: ").title()
             while True:
                 try:
@@ -154,8 +154,9 @@ def ler_livros():
         return()
     else:
         os.system("cls")
+        print("="*9)
 #printa o livro e suas informações (tem que se referir assim mesmo, porque a forma de importar o arquivo deixa ele assim)
-        print(f" nome: {lista_de_leitura[ação]}\n genero: {biblioteca[lista_de_leitura[ação]][0]} \n autor: {biblioteca[lista_de_leitura[ação]][1]} \n preço: {biblioteca[lista_de_leitura[ação]][2]} \n")
+        print(f"Nome: {lista_de_leitura[ação]} \nGenero: {biblioteca[lista_de_leitura[ação]][0]} \nAutor: {biblioteca[lista_de_leitura[ação]][1]} \nValor: {biblioteca[lista_de_leitura[ação]][2]} \n")
 
     while True:
         retorno = (input("\n\nDigite [0] para voltar ao menu \n\n")).upper()
@@ -188,13 +189,14 @@ def atualizar_livros():
             print("Valor inválido, digite um valor válido")
             continue
     
-    print("\n="*9)
+    print()
+    print("="*9)
     print("\tSecção de alterar informações \nQual informação deseja alterar? (digite apenas numeros numeros)")
-    print("\n[0]- Alterar Nome \n[1]- Alterar Categoria \n[2]- Alterar Autor \n[3]- Alterar valor")
-    #UnboundLocalError
+    print("\n[0]- Alterar Nome \n[1]- Alterar Gênero \n[2]- Alterar Autor \n[3]- Alterar valor")
+
     while True:
         try:
-            seccao = int(input())
+            seccao = int(input("- "))
 
             if seccao > 3 or seccao < 0:
                 print("Secção inválida, digite uma secção válida")
@@ -208,16 +210,18 @@ def atualizar_livros():
             break
     
     if seccao == 0: #Alterar Nome
-        print("\n="*9)
+        print()
+        print("="*9)
         print(f"Escreva o novo nome do livro {key}: ")
         value = input("- ").title()
 
         biblioteca[value] = biblioteca[key]
         del biblioteca[key]
 
-    elif seccao == 1: #Alterar Categoria
-        print("\n="*9)
-        print(f"Escreva a nova categoria do livro {key}: ")
+    elif seccao == 1: #Alterar Genero
+        print()
+        print("="*9)
+        print(f"Escreva o novo gênero do livro {key}: ")
         value = input("- ").title()
         for i in range(len(biblioteca[key])):
             if i+1 == seccao:
@@ -225,7 +229,8 @@ def atualizar_livros():
                 biblioteca[key][i] = value
 
     elif seccao == 2: #Alterar Autor
-        print("\n="*9)
+        print()
+        print("="*9)
         print(f"Escreva o novo autor do livro {key}: ")
         value = input("- ").title()
         for i in range(len(biblioteca[key])):
@@ -234,7 +239,8 @@ def atualizar_livros():
                 biblioteca[key][i] = value
 
     elif seccao == 3: #Alterar Valor
-        print("\n="*9)
+        print()
+        print("="*9)
         while True:
             print(f"Escreva o valor novo do livro {key}")
             value = input("- ").title()
@@ -263,7 +269,8 @@ def atualizar_livros():
 #Função Delete/Remover livro do registro
 def remover_livros():
     
-    print("Qual livro deseja retirar da biblioteca? (digite [0] para sair) ")
+    print("\tRemover livros\n")
+    print("Qual livro deseja retirar da biblioteca? \nPara voltar ao menu principal digite [0]\n")
     while True:
         try:
             key = input("Livro: ").title()
@@ -275,6 +282,7 @@ def remover_livros():
                 print("Livro não encontrado, tente novamente")
                 continue
         except KeyError:
+            print("Livro não encontrado, tente novamente")   
             continue
     
     del biblioteca[key]
@@ -288,6 +296,7 @@ def remover_livros():
     file.close()
 
 #===================
+#Funcionalidade extra - Recomendaçõs
 def recomendations():
     file = open('recomend.txt', 'r', encoding='utf8')
 
@@ -305,19 +314,26 @@ def recomendations():
         lista1[j] = lista1[j].split(',')
 
         recomendacoes[lista1[j][0]] = [lista1[j][1:]]
+
     while True: 
         gender = input("Digite o genero que deseja recomendações: ").title() #escolhendo o genero para achar recomendações
         
         if gender in recomendacoes:
 
+
+            print()
+            print("="*9)            
             print(f"Recomendações do genero {gender}: \n")
 
-            num=0
+            num = 0
             for item in recomendacoes.get(gender)[0]: #printando as recomendações
                 if item not in biblioteca.keys():
                     num+=1
                     print(f"{num}. {item}")
 
+
+            print()
+            print("="*9)            
             continuar = input("Deseja continuar procurando recomendações? [S/N] ").upper() #perguntando se a função será reiniciada
             if continuar == "N":
                 break
@@ -332,6 +348,9 @@ def recomendations():
             os.system('cls')
             print("Genero não encontrado")
             while True:
+                
+                print()
+                print("="*9) 
                 continuar = input("Deseja continuar procurando recomendações? [S/N] ").upper() #perguntando se a função será reiniciada
                 if continuar == "N":
                     os.system("cls")
@@ -344,8 +363,10 @@ def recomendations():
                     print("Comando invalido, digite S ou N")
 
 #=================
+#Função de filtros - menu de filtros
 def MenuDeFiltros():
-    comando_invalido = False #Esse boleano checa se o usuário mandou um comando inválido. Se sim, loopa o código e manda uma mensagem de erro
+    #Esse boleano checa se o usuário mandou um comando inválido. Se sim, loopa o código e manda uma mensagem de erro
+    comando_invalido = False 
     while True:
         os.system ("cls")
 
@@ -353,7 +374,9 @@ def MenuDeFiltros():
         if comando_invalido == True:
             os.system ("cls")
             print("ERRO: Comando inválido! Por favor coloque um número de [0 a 3]")
-            comando_invalido = False #Isso garante que o código não vai loopar infinitamente
+
+            #Isso garante que o código não vai loopar infinitamente
+            comando_invalido = False 
 
         print ("Qual filtro gostaria de usar na sua pesquisa?\n")
         filtro = input("(Para retornar ao menu principal digite 0)\n\n 1. Gênero\n 2. Autor\n 3. Preço \n\n").lower()
@@ -366,7 +389,8 @@ def MenuDeFiltros():
             FiltroPorPreco()
         elif filtro == '0':
             os.system("cls")
-            break #Aqui onde será a volta ao menu principal, mas ele não existe nesse código ainda
+            #Aqui onde será a volta ao menu principal, mas ele não existe nesse código ainda
+            break 
         else:
             comando_invalido = True
             continue
@@ -379,10 +403,9 @@ def FiltroPorGenero():
 
         print('Você está na sessão de filtro por gênero!')
         genero = input("Por qual gênero você deseja filtrar?\n\n").title() #Input do usuário
-        # if genero == "Hq":
-        #     genero = genero.upper() #Pra que funcione com esse gênero porque ele apresenta duas letras maiúsculas
-
-        livros_filtrados.clear() #Isso garante que quaiser livros de uma filtração anterior são excluídos para a próxima filtração
+        
+        #Isso garante que quaiser livros de uma filtração anterior são excluídos para a próxima filtração
+        livros_filtrados.clear() 
 
         os.system ("cls")
         for livro, valores in biblioteca.items(): 
